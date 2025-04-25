0c153b3a3856555b754cbf6b6fc7ccb55447efd7
gunner <gymgunner@gmail.com>
2025-04-25 11:45:24 +0800

    Initial Commit

---
+++ SpatialGestureDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SpatialGestureDemo
+//
+//  Created by GUNNER on 2025/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ SpatialGestureDemo/SpatialGestureDemoApp.swift
@@ -0,0 +1,33 @@
+//
+//  SpatialGestureDemoApp.swift
+//  SpatialGestureDemo
+//
+//  Created by GUNNER on 2025/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SpatialGestureDemoApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
